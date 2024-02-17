bc907022d973b674c3a62b0b34ed41c82eeab850
sarah2244-4 <douthwaite.sarah@yahoo.co.uk>
2024-02-17 22:42:03 +0000

    Replaced secret key

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+if os.path.isfile('env.py'):
+    import env
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x+4n9t(td+t_th)$-jsr5t1q0_jw152s2l+)%2nz!1#+8$_$n2'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
